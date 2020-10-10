917b83421d97d86d86774c516d0c5ec1549b5404
EvanSamuelW <evanwiyendra@gmail.com>
2020-10-10 12:53:08 +0700

    master file

---
+++ app/src/main/java/com/evansamuel/pbptubes/EditProfile.java
@@ -0,0 +1,171 @@
+package com.evansamuel.pbptubes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class EditProfile extends AppCompatActivity {
+
+    public static final String TAG = "TAG";
+    EditText profileUsername,profileName,profileAddress,profileEmail,profilePhone;
+    ImageView profileImageView;
+    Button saveBtn;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    FirebaseUser user;
+    StorageReference storageReference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+
+        Intent data = getIntent();
+        String fullName = data.getStringExtra("fName");
+        String address = data.getStringExtra("alamat");
+        String email = data.getStringExtra("email");
+        String phone = data.getStringExtra("telp");
+        String username = data.getStringExtra("username");
+
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+        user = fAuth.getCurrentUser();
+        storageReference = FirebaseStorage.getInstance().getReference();
+
+        profileUsername = findViewById(R.id.profileUsername);
+        profileName = findViewById(R.id.profileName);
+        profileAddress = findViewById(R.id.profileAddress);
+        profileEmail = findViewById(R.id.profileEmail);
+        profilePhone = findViewById(R.id.profileTelp);
+        profileImageView = findViewById(R.id.profileImageView);
+        saveBtn = findViewById(R.id.saveProfileInfo);
+
+        StorageReference profileRef = storageReference.child("users/"+fAuth.getCurrentUser().getUid()+"/profile.jpg");
+        profileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Picasso.get().load(uri).into(profileImageView);
+            }
+        });
+
+        profileImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent openGalleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(openGalleryIntent,1000);
+            }
+        });
+
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (profileUsername.getText().toString().isEmpty() || profileName.getText().toString().isEmpty() || profileAddress.getText().toString().isEmpty() || profileEmail.getText().toString().isEmpty() || profilePhone.getText().toString().isEmpty()) {
+                    Toast.makeText(EditProfile.this, "One or Many fields are empty.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                final String email = profileEmail.getText().toString();
+                user.updateEmail(email).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        DocumentReference docRef = fStore.collection("users").document(user.getUid());
+                        Map<String, Object> edited = new HashMap<>();
+                        edited.put("fName", profileName.getText().toString());
+                        edited.put("alamat", profileAddress.getText().toString());
+                        edited.put("email", email);
+                        edited.put("telp", profilePhone.getText().toString());
+                        edited.put("Username", profileUsername.getText().toString());
+                        docRef.update(edited).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Toast.makeText(EditProfile.this, "Profile Updated", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                                finish();
+                            }
+                        });
+                        Toast.makeText(EditProfile.this, "Email is changed.", Toast.LENGTH_SHORT).show();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(EditProfile.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+        });
+
+        profileEmail.setText(email);
+        profileUsername.setText(username);
+        profileName.setText(fullName);
+        profilePhone.setText(phone);
+        profileAddress.setText(address);
+
+        Log.d(TAG, "onCreate: " + username + " " + fullName + " " + phone + " " + address + " " + email);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @androidx.annotation.Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1000){
+            if(resultCode == Activity.RESULT_OK){
+                Uri imageUri = data.getData();
+
+                //profileImage.setImageURI(imageUri);
+
+                uploadImageToFirebase(imageUri);
+
+
+            }
+        }
+
+    }
+
+    private void uploadImageToFirebase(Uri imageUri) {
+        // uplaod image to firebase storage
+        final StorageReference fileRef = storageReference.child("users/"+fAuth.getCurrentUser().getUid()+"/profile.jpg");
+        fileRef.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                fileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        Picasso.get().load(uri).into(profileImageView);
+                    }
+                });
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getApplicationContext(), "Failed.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/evansamuel/pbptubes/ProfileActivity.java
@@ -0,0 +1,114 @@
+package com.evansamuel.pbptubes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.webkit.MimeTypeMap;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
+
+import javax.annotation.Nullable;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    private static final int GALLERY_INTENT_CODE = 1023 ;
+    TextView fullName,username,alamat,email,phone;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String userId;
+    FirebaseUser user;
+    ImageView profileImage;
+    StorageReference storageReference;
+    Button changeProfileImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        username = findViewById(R.id.edtUsername);
+        fullName = findViewById(R.id.profileName);
+        alamat = findViewById(R.id.profileAddress);
+        phone = findViewById(R.id.profileTelp);
+        email    = findViewById(R.id.profileEmail);
+
+        profileImage = findViewById(R.id.profileImage);
+        changeProfileImage = findViewById(R.id.changeProfile);
+
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+        storageReference = FirebaseStorage.getInstance().getReference();
+
+        StorageReference profileRef = storageReference.child("users/"+fAuth.getCurrentUser().getUid()+"/profile.jpg");
+        profileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Picasso.get().load(uri).into(profileImage);
+            }
+        });
+
+        userId = fAuth.getCurrentUser().getUid();
+        user = fAuth.getCurrentUser();
+
+        final DocumentReference documentReference = fStore.collection("users").document(userId);
+        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+                if(documentSnapshot.exists()){
+                    username.setText(documentSnapshot.getString("username"));
+                    fullName.setText(documentSnapshot.getString("fName"));
+                    phone.setText(documentSnapshot.getString("telp"));
+                    alamat.setText(documentSnapshot.getString("alamat"));
+                    email.setText(documentSnapshot.getString("email"));
+
+                }else {
+                    Log.d("tag", "onEvent: Document do not exists");
+                }
+            }
+        });
+
+        changeProfileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(v.getContext(), EditProfile.class);
+                i.putExtra("fName", fullName.getText().toString());
+                i.putExtra("alamat", alamat.getText().toString());
+                i.putExtra("email", email.getText().toString());
+                i.putExtra("telp", phone.getText().toString());
+                i.putExtra("username", username.getText().toString());
+                startActivity(i);
+            }
+        });
+    }
+
+}
+
