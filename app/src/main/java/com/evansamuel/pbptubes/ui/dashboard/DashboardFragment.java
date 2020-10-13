11bf50939ab291077be60d5b2609f2e1a80f3f29
WilliamLourensius <wlourensius@gmail.com>
2020-10-13 14:20:28 +0700

    William

--- app/src/main/java/com/evansamuel/pbptubes/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/evansamuel/pbptubes/ui/dashboard/DashboardFragment.java
@@ -1,10 +1,19 @@
 package com.evansamuel.pbptubes.ui.dashboard;
 
+import android.Manifest;
 import android.app.FragmentManager;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.cardview.widget.CardView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.Navigation;
@@ -16,18 +25,21 @@ import android.view.ViewGroup;
 import com.evansamuel.pbptubes.R;
 import com.evansamuel.pbptubes.ui.fitur.BookFragment;
 
+import static android.content.Context.LOCATION_SERVICE;
+import static androidx.core.content.ContextCompat.getSystemService;
+
 
 public class DashboardFragment extends Fragment {
 
     private CardView cardHome,cardFood,cardLocation;
     private final static String TAG_FRAGMENT = "TAG_FRAGMENT";
-
+    Context context;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
+    static public final int REQUEST_LOCATION = 1;
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
@@ -86,12 +98,32 @@ public class DashboardFragment extends Fragment {
         cardLocation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Navigation.findNavController(view).navigate(R.id.action_nav_dashboard_to_nav_location);
-
+                if (ActivityCompat.checkSelfPermission(getActivity(),
+                        Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+                }
+                else
+                {
+                    Navigation.findNavController(view).navigate(R.id.action_nav_dashboard_to_nav_location);
+                }
             }
         });
         return view;
 
     }
+    
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,String[] permissions, int[] grantResults) {
+        if (requestCode == REQUEST_LOCATION) {
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+            } else {
+                // Permission was denied or request was cancelled
+            }
+        }
+    }
+
 
 }
