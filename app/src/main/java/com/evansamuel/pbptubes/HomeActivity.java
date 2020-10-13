4c37217d11033796d2fea1a62c582c4d7d6aeb24
WilliamLourensius <wlourensius@gmail.com>
2020-10-13 19:27:41 +0700

    William

--- app/src/main/java/com/evansamuel/pbptubes/HomeActivity.java
+++ app/src/main/java/com/evansamuel/pbptubes/HomeActivity.java
@@ -122,7 +122,7 @@ public class HomeActivity extends AppCompatActivity {
         // menu should be considered as top level destinations.
 
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_dashboard,R.id.nav_profile)
+                R.id.nav_dashboard,R.id.nav_profile,R.id.nav_logout)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
